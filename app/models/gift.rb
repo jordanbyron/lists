72e47f8a3bf36bccdbdea050b3093708cba0c2a6
Jordan Byron <jordan.byron@gmail.com>
2013-11-23 19:09:41 -0500

    Limit Shopping list

--- app/models/gift.rb
+++ app/models/gift.rb
@@ -8,33 +8,37 @@ class Gift < ActiveRecord::Base
             GROUP BY gifts.id) AS c ON gifts.id = c.id}).
       where("gifts.quantity > c.total_claims")
   end
-  
+
   def self.active
     includes(:list).where('lists.archived = false')
   end
-  
+
+  def self.upcoming_and_recent
+    includes(:list).where("lists.occurs_on >= ?", Date.today + 1.week)
+  end
+
   def self.claimed_by(user)
     includes(:claims).where("claims.user_id = ?", user.id)
   end
-  
+
   def self.not_claimed_by(user)
     includes(:claims).where("claims.user_id != ?", user.id)
   end
-  
+
   def claim(user)
     Claim.create(:user => user, :gift => self)
   end
-  
+
   def return_to_list(user)
     claims.find_by_user_id(user).destroy
   end
-  
+
   def available?
     claims.count < quantity
   end
-  
+
   def claimed_by?(user)
     claims.where(:user_id => user).any?
   end
-  
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ActiveRecord::Base
   before_save :reset_perishable_token
-  
+
   has_many :authorizations, :dependent => :destroy
   has_many :lists,          :dependent => :destroy
   has_many :invites,        :dependent => :destroy
-  
+
   has_many :claims,         :dependent => :destroy
 
   attr_protected :admin
@@ -15,15 +15,16 @@ class User < ActiveRecord::Base
       email: hash['info']['email']
     )
   end
-  
+
   def invited_lists
     List.includes(:invites).where("invites.user_id = ?", id)
   end
-  
+
   def shopping_list
-    Gift.claimed_by(self).active.order("claims.purchased, lists.name, gifts.name")
+    Gift.claimed_by(self).active.upcoming_and_recent.
+      order("claims.purchased, lists.name, gifts.name")
   end
-  
+
   def reset_perishable_token
     self.perishable_token = SecureRandom.hex(12)
   end
