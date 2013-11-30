f4586a9667c065b59091eb3f5155ae013c49b8c5
Jordan Byron <jordan.byron@gmail.com>
2013-11-30 09:29:24 -0500

    Social Icons :metal:

--- config/application.rb
+++ config/application.rb
@@ -53,6 +53,8 @@ module Lists
 
     config.assets.initialize_on_precompile = false
 
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+
     # Default uses divs which rendering issues with inline labels
     ActionView::Base.field_error_proc = Proc.new { |html_tag, instance|
       "<span class=\"field_with_errors\">#{html_tag}</span>".html_safe
