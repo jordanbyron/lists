a6526a248d027a28c3941cbf813957ccdd76d34b
Jordan Byron <jordan.byron@gmail.com>
2014-12-07 18:25:07 -0500

    Don't allow nil key lookups

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,6 +1,6 @@
 class AccountsController < ApplicationController
   before_filter :find_user, :except => [:setup]
-  
+
   def update
     if @user.update_attributes(params[:user])
       if @user.authorizations.empty?
@@ -13,28 +13,34 @@ class AccountsController < ApplicationController
       render :show
     end
   end
-  
+
   def setup
-    @user = User.find_by_perishable_token(params[:key])
-    if @user.blank? || !@user.authorizations.empty?
-      flash[:error] = %{Sorry, this key is no longer valid or your account is 
-                        already setup}
-      redirect_to root_path
+    if params[:key].present?
+      @user = User.find_by_perishable_token(params[:key])
+      if @user.blank? || !@user.authorizations.empty?
+        flash[:error] = %{Sorry, this key is no longer valid or your account is
+                          already setup}
+        redirect_to root_path
+      else
+        self.current_user = @user
+        @user.save
+      end
     else
-      self.current_user = @user
-      @user.save
+      flash[:error] = %{We're sorry, but it looks like the link you are using
+      is missing some information. Please ask the person who invited you to
+      re-send you invitation}
+      redirect_to root_path
     end
   end
-  
+
   def shopping_list
     @gifts = GiftDecorator.decorate(@user.shopping_list)
   end
-  
+
   private
-  
+
   def find_user
     @user           = current_user
     @authorizations = @user.authorizations.where("provider != 'identity'")
   end
-  
 end
--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -2,7 +2,11 @@ class InviteMailer < ActionMailer::Base
   def new_invite(invite)
     @invite = invite
     @list   = ListDecorator.decorate(invite.list)
-    
+
+    @user = @invite.user
+    @user.reset_perishable_token
+    @user.save
+
     mail(:to      => invite.user.email,
          :from    => "#{@list.user.name} <donotreply@simplegiftlist.com>",
          :subject => "You've been invited to view #{@list.name}").deliver
